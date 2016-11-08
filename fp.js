4a8e6a5108a536db1ae65f39692065a21ca1058e
AndyRoyal <419656532@qq.com>
2016-11-08 10:49:56 +0800

    随码

--- fp.js
+++ fp.js
@@ -1,3 +1,46 @@
+ 
+//随码1：
+ function fn(param1,param2,param3){
+    if(isfn(param1) && isfn(param2) && isfn(param3)){
+        return [
+            param1,param2,param3
+        ]
+    }
+    if(iso(param1) && iso(param2) && iso(param3)){
+        return  {
+            o1:param1
+            ,o2:param2
+            ,o3:param3
+        }
+    }
+    
+ };
+ function isfn(a){
+    return a == null ? false:
+        Object.prototype.toString.call(a) == '[object Function]';
+ }
+
+ function iso(a){
+     return a == null ? false
+         :Object.prototype.toString.call(a) == "[object Object]";
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
  // scope function
     function isarray(a){
         return a == null ? false
